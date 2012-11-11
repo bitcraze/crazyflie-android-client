4fbb0dd016090ed306d6bff4e0a09d7159a3f159
Arnaud Taffanel <unknown>
2012-11-11 16:57:10 +0100

    Connect to the radio and sends a NULL parcket cyclically.

--- gen/se/bitcraze/crazyfliecontrol/R.java
+++ gen/se/bitcraze/crazyfliecontrol/R.java
@@ -15,26 +15,29 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int joysticks=0x7f070000;
-        public static final int menu_connect=0x7f070001;
-        public static final int menu_disconnect=0x7f070002;
+        public static final int joysticks=0x7f080000;
+        public static final int menu_connect=0x7f080001;
+        public static final int menu_disconnect=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int connect=0x7f040004;
-        public static final int copter_channel=0x7f040005;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_connect=0x7f040006;
-        public static final int menu_disconnect=0x7f040002;
-        public static final int title_activity_main=0x7f040003;
+        public static final int app_name=0x7f050000;
+        public static final int connect=0x7f050004;
+        public static final int copter_channel=0x7f050005;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_connect=0x7f050006;
+        public static final int menu_disconnect=0x7f050002;
+        public static final int title_activity_main=0x7f050003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
+    }
+    public static final class xml {
+        public static final int device_filter=0x7f040000;
     }
 }
--- src/se/bitcraze/crazyfliecontrol/MainActivity.java
+++ src/se/bitcraze/crazyfliecontrol/MainActivity.java
@@ -3,12 +3,34 @@ package se.bitcraze.crazyfliecontrol;
 import com.MobileAnarchy.Android.Widgets.Joystick.DualJoystickView;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.hardware.usb.UsbConstants;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbEndpoint;
+import android.hardware.usb.UsbInterface;
+import android.hardware.usb.UsbManager;
+import android.util.Log;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements Runnable{
+	
+    private static final String TAG = "CrazyflieControl";
 
-	DualJoystickView joysticks;
+	private DualJoystickView mJoysticks;
+	private UsbManager mUsbManager;
+
+	private Object mDevice;
+
+	private UsbEndpoint mEndpointIntr;
+
+	private UsbEndpoint mEpIn;
+
+	private UsbEndpoint mEpOut;
+
+	private UsbDeviceConnection mConnection;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -16,7 +38,8 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     
-        this.joysticks = (DualJoystickView) findViewById(R.id.joysticks);
+        mJoysticks = (DualJoystickView) findViewById(R.id.joysticks);
+        mUsbManager = (UsbManager)getSystemService(Context.USB_SERVICE);
     }
 
     @Override
@@ -24,4 +47,87 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	
+    	Intent intent = getIntent();
+        Log.d(TAG, "intent: " + intent);
+        String action = intent.getAction();
+        
+        UsbDevice device = (UsbDevice)intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+        if (UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action)) {
+            setDevice(device);
+        } else if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
+            if (mDevice != null && mDevice.equals(device)) {
+                setDevice(null);
+            }
+        }
+    }
+    
+    private void setDevice(UsbDevice device) {
+        Log.d(TAG, "setDevice " + device);
+        if (device.getInterfaceCount() != 1) {
+            Log.e(TAG, "could not find interface");
+            return;
+        }
+        UsbInterface intf = device.getInterface(0);
+        // device should have two endpoint
+        if (intf.getEndpointCount() != 2) {
+            Log.e(TAG, "could not find endpoint");
+            return;
+        }
+        // endpoints should be of type bulk
+        UsbEndpoint ep = intf.getEndpoint(0);
+        if (ep.getType() != UsbConstants.USB_ENDPOINT_XFER_BULK) {
+            Log.e(TAG, "endpoint is not bulk type");
+            return;
+        }
+        mDevice = device;
+        
+        if (ep.getDirection()==UsbConstants.USB_DIR_IN) {
+        	mEpIn = intf.getEndpoint(0);
+        	mEpOut = intf.getEndpoint(1);
+        } else {
+        	mEpIn = intf.getEndpoint(0);
+        	mEpOut = intf.getEndpoint(1);
+        }
+        
+        if (device != null) {
+            UsbDeviceConnection connection = mUsbManager.openDevice(device);
+            if (connection != null && connection.claimInterface(intf, true)) {
+                Log.d(TAG, "open SUCCESS");
+                mConnection = connection;
+                Thread thread = new Thread(this);
+                thread.start();
+
+            } else {
+                Log.d(TAG, "open FAIL");
+                mConnection = null;
+            }
+         }
+    }
+   
+    //Runs the communication loop in a thread...
+	public void run() {
+		//Set channel 107
+		mConnection.controlTransfer(0x40, 0x01, 107, 0, null, 0, 100);
+		//Set datarate 250K
+		mConnection.controlTransfer(0x40, 0x03,   0, 0, null, 0, 100);
+		
+		while (mDevice != null) {
+			byte [] data = new byte[33];
+			
+			data[0] = (byte) 0xff;
+			
+        	mConnection.bulkTransfer(mEpOut, data, 1, 100);
+        	mConnection.bulkTransfer(mEpIn, data, 33, 100);
+			try {
+				Thread.sleep(20, 0);
+			} catch (InterruptedException e) {
+				;
+			}
+		}
+	}
 }
