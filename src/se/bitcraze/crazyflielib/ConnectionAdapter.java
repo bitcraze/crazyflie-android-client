f148bd22963b849351b4215675cd011c4f95f803
ssams <unknown>
2013-07-17 19:26:43 +0200

    style changes - missed a file

--- src/se/bitcraze/crazyflielib/ConnectionAdapter.java
+++ src/se/bitcraze/crazyflielib/ConnectionAdapter.java
@@ -11,37 +11,58 @@ package se.bitcraze.crazyflielib;
  */
 public abstract class ConnectionAdapter implements ConnectionListener {
 
-	/* (non-Javadoc)
-	 * @see se.bitcraze.crazyflielib.ConnectionListener#connectionInitiated(se.bitcraze.crazyflielib.Link)
-	 */
-	@Override
-	public void connectionInitiated(Link l) {}
+    /*
+     * (non-Javadoc)
+     * @see
+     * se.bitcraze.crazyflielib.ConnectionListener#connectionInitiated(se.bitcraze
+     * .crazyflielib.Link)
+     */
+    @Override
+    public void connectionInitiated(Link l) {
+    }
 
-	/* (non-Javadoc)
-	 * @see se.bitcraze.crazyflielib.ConnectionListener#connectionSetupFinished(se.bitcraze.crazyflielib.Link)
-	 */
-	@Override
-	public void connectionSetupFinished(Link l) {}
+    /*
+     * (non-Javadoc)
+     * @see
+     * se.bitcraze.crazyflielib.ConnectionListener#connectionSetupFinished(se
+     * .bitcraze.crazyflielib.Link)
+     */
+    @Override
+    public void connectionSetupFinished(Link l) {
+    }
 
-	/* (non-Javadoc)
-	 * @see se.bitcraze.crazyflielib.ConnectionListener#disconnected(se.bitcraze.crazyflielib.Link)
-	 */
-	@Override
-	public void disconnected(Link l) {}
+    /*
+     * (non-Javadoc)
+     * @see
+     * se.bitcraze.crazyflielib.ConnectionListener#disconnected(se.bitcraze.
+     * crazyflielib.Link)
+     */
+    @Override
+    public void disconnected(Link l) {
+    }
 
-	/* (non-Javadoc)
-	 * @see se.bitcraze.crazyflielib.ConnectionListener#connectionLost(se.bitcraze.crazyflielib.Link)
-	 */
-	@Override
-	public void connectionLost(Link l) {}
+    /*
+     * (non-Javadoc)
+     * @see
+     * se.bitcraze.crazyflielib.ConnectionListener#connectionLost(se.bitcraze
+     * .crazyflielib.Link)
+     */
+    @Override
+    public void connectionLost(Link l) {
+    }
 
-	/* (non-Javadoc)
-	 * @see se.bitcraze.crazyflielib.ConnectionListener#connectionFailed(se.bitcraze.crazyflielib.Link)
-	 */
-	@Override
-	public void connectionFailed(Link l) {}
+    /*
+     * (non-Javadoc)
+     * @see
+     * se.bitcraze.crazyflielib.ConnectionListener#connectionFailed(se.bitcraze
+     * .crazyflielib.Link)
+     */
+    @Override
+    public void connectionFailed(Link l) {
+    }
 
-	@Override
-	public void linkQualityUpdate(Link l, int quality) {}
+    @Override
+    public void linkQualityUpdate(Link l, int quality) {
+    }
 
 }
