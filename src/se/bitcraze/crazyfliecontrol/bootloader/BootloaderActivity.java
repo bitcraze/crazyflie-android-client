821ec0c641c0039ab4478105a01d6fecd40ae1b8
Fred G <fredg02@hotmail.com>
2015-10-15 02:14:18 +0200

    Extract FirmwareDownloader

--- src/se/bitcraze/crazyfliecontrol/bootloader/BootloaderActivity.java
+++ src/se/bitcraze/crazyfliecontrol/bootloader/BootloaderActivity.java
@@ -1,22 +1,7 @@
 package se.bitcraze.crazyfliecontrol.bootloader;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import se.bitcraze.crazyfliecontrol.bootloader.Firmware.Asset;
 import se.bitcraze.crazyfliecontrol2.R;
@@ -26,16 +11,13 @@ import se.bitcraze.crazyflielib.bootloader.Bootloader.BootloaderListener;
 import se.bitcraze.crazyflielib.bootloader.Utilities.BootVersion;
 import se.bitcraze.crazyflielib.crazyradio.RadioDriver;
 import android.app.Activity;
-import android.app.DownloadManager;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -57,9 +39,7 @@ public class BootloaderActivity extends Activity {
     private TextView mStatusLineTextView;
 
     private Firmware mSelectedFirmware = null;
-    private String mDownloadDirectory = "CrazyflieControl";
-
-    private List<Firmware> mFirmwares = new ArrayList<Firmware>();
+    private FirmwareDownloader mFirmwareDownloader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,130 +52,52 @@ public class BootloaderActivity extends Activity {
         mStatusLineTextView = (TextView) findViewById(R.id.bootloader_statusLine);
 
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+
+        mFirmwareDownloader = new FirmwareDownloader(this);
     }
 
-
     public void checkForFirmwareUpdate(View view) {
-        String releaseURL = "https://api.github.com/repos/bitcraze/crazyflie-firmware/releases";
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
             mStatusLineTextView.setText("Status: Checking for updates...");
             mCheckUpdateButton.setEnabled(false);
-            new DownloadWebpageTask().execute(releaseURL);
+            mFirmwareDownloader.checkForFirmwareUpdate();
         } else {
             mStatusLineTextView.setText("Status: No internet connection available.");
         }
     }
 
-    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+    public void updateFirmwareSpinner(List<Firmware> firmwares) {
+        mCheckUpdateButton.setEnabled(true);
+        if (!firmwares.isEmpty()) {
+            mFirmwareSpinner.setVisibility(View.VISIBLE);
+            ArrayAdapter<Firmware> dataAdapter = new ArrayAdapter<Firmware>(BootloaderActivity.this, android.R.layout.simple_spinner_item, firmwares);
+            dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            mFirmwareSpinner.setAdapter(dataAdapter);
+            mFirmwareSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
 
-        @Override
-        protected String doInBackground(String... urls) {
-
-            // params comes from the execute() call: params[0] is the url.
-            try {
-                String input = downloadUrl(urls[0]);
-                mFirmwares = parseJson(input);
-
-                return "Status: Found " + mFirmwares.size() + " firmwares.";
-            } catch (IOException e) {
-                return "Unable to retrieve web page. URL may be invalid.";
-            } catch (JSONException e) {
-                return "Error during parsing JSON content.";
-            }
-        }
-        // onPostExecute displays the results of the AsyncTask.
-        @Override
-        protected void onPostExecute(String result) {
-            mCheckUpdateButton.setEnabled(true);
-            if (!mFirmwares.isEmpty()) {
-                mFirmwareSpinner.setVisibility(View.VISIBLE);
-                ArrayAdapter<Firmware> dataAdapter = new ArrayAdapter<Firmware>(BootloaderActivity.this, android.R.layout.simple_spinner_item, mFirmwares);
-                dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                mFirmwareSpinner.setAdapter(dataAdapter);
-                mFirmwareSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
-
-                    @Override
-                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                        Firmware firmware = (Firmware) mFirmwareSpinner.getSelectedItem();
-                        mSelectedFirmware = firmware;
-                        mFlashFirmwareButton.setEnabled(true);
-                    }
-
-                    @Override
-                    public void onNothingSelected(AdapterView<?> parent) {
-                        mFlashFirmwareButton.setEnabled(false);
-                    }
-
-                });
-            } else {
-                //TODO
-            }
-            mStatusLineTextView.setText(result);
-       }
-    }
-
-    private String downloadUrl(String myUrl) throws IOException {
-        BufferedReader reader = null;
-        StringBuilder builder = new StringBuilder();
-        HttpClient client = new DefaultHttpClient();
-        HttpGet httpGet = new HttpGet(myUrl);
-        try {
-            HttpResponse response = client.execute(httpGet);
-            StatusLine statusLine = response.getStatusLine();
-            int statusCode = statusLine.getStatusCode();
-
-            if (statusCode == 200) {
-                HttpEntity entity = response.getEntity();
-                InputStream content = entity.getContent();
-                reader = new BufferedReader(new InputStreamReader(content, "UTF-8"));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    builder.append(line);
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    Firmware firmware = (Firmware) mFirmwareSpinner.getSelectedItem();
+                    mSelectedFirmware = firmware;
+                    mFlashFirmwareButton.setEnabled(true);
                 }
-            } else {
-                Log.d(LOG_TAG, "The response is: " + response);
-                return "The response is: " + response;
-            }
-        } finally {
-            // Makes sure that the InputStream is closed after the app is finished using it.
-            if (reader != null) {
-                reader.close();
-            }
-        }
-        return builder.toString();
-    }
 
-    public List<Firmware> parseJson(String input) throws JSONException {
-
-        List<Firmware> firmwares = new ArrayList<Firmware>();
-
-        JSONArray releasesArray = new JSONArray(input);
-        for (int i = 0; i < releasesArray.length(); i++) {
-            JSONObject releaseObject = releasesArray.getJSONObject(i);
-            String tagName = releaseObject.getString("tag_name");
-            String name = releaseObject.getString("name");
-            String createdAt = releaseObject.getString("created_at");
-            Firmware firmware = new Firmware(tagName, name, createdAt);
-            JSONArray assetsArray = releaseObject.getJSONArray("assets");
-            if (assetsArray != null && assetsArray.length() > 0) {
-                for (int n = 0; n < assetsArray.length(); n++) {
-                    JSONObject assetsObject = assetsArray.getJSONObject(n);
-                    String assetName = assetsObject.getString("name");
-                    int size = assetsObject.getInt("size");
-                    String downloadURL = assetsObject.getString("browser_download_url");
-                    Asset asset = new Firmware().new Asset(assetName, size, downloadURL);
-                    firmware.addAsset(asset);
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                    mFlashFirmwareButton.setEnabled(false);
                 }
-            }
-            firmwares.add(firmware);
+
+            });
+        } else {
+            //TODO
         }
-        return firmwares;
     }
 
-
+    public void setStatusLine (String status) {
+        this.mStatusLineTextView.setText(status);
+    }
 
     public void flashFirmware(View view) {
         //TODO: enable wakelock
@@ -205,50 +107,16 @@ public class BootloaderActivity extends Activity {
         mFlashFirmwareButton.setEnabled(false);
         mFirmwareSpinner.setEnabled(false);
 
+        // TODO: not visible
         mStatusLineTextView.setText("Downloading firmware...");
 
-        if (this.mSelectedFirmware != null && mSelectedFirmware.getAssets().size() > 0) {
-            for (Asset asset : this.mSelectedFirmware.getAssets()) {
-                //check if file is already downloaded
-                if (isFileAlreadyDownloaded(asset)) {
-                    Log.d(LOG_TAG, "File " + asset.getName() + " already downloaded.");
-                } else {
-                    String browserDownloadUrl = asset.getBrowserDownloadUrl();
-                    downloadFile(browserDownloadUrl, asset.getName());
-                }
-            }
-        } else {
-            mStatusLineTextView.setText("Selected firmware does not have assets.");
-            return;
-        }
+        mFirmwareDownloader.downloadFirmware(this.mSelectedFirmware);
 
         Toast.makeText(this, "Flashing firmware...", Toast.LENGTH_SHORT).show();
-
         new FlashFirmwareTask().execute();
     }
 
-    private boolean isFileAlreadyDownloaded (Asset asset) {
-        int assetSize = asset.getSize();
-        File sdcard = Environment.getExternalStorageDirectory();
-        File firmwareFile = new File(sdcard, mDownloadDirectory + "/" + asset.getName());
-        return firmwareFile.exists() && firmwareFile.length() == assetSize;
-    }
 
-    public void downloadFile (String url, String fileName) {
-        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-        request.setDescription("Some description");
-        request.setTitle(fileName);
-        // in order for this if to run, you must use the android 3.2 to compile your app
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            request.allowScanningByMediaScanner();
-            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-        }
-        request.setDestinationInExternalPublicDir(mDownloadDirectory, fileName);
-
-        // get download service and enqueue file
-        DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
-        manager.enqueue(request);
-    }
 
     private class FlashFirmwareTask extends AsyncTask<String, String, String> {
 
@@ -291,22 +159,20 @@ public class BootloaderActivity extends Activity {
                     Log.d(LOG_TAG, "Found Crazyflie " + (cfType2 ? "2.0" : "1.0") + ".");
 
                     //TODO: deal with Zip files
-                    String fileName = null;
+                    Asset selectedAsset = null;
                     if (mSelectedFirmware != null && mSelectedFirmware.getAssets().size() > 0) {
                         for (Asset asset : mSelectedFirmware.getAssets()) {
                             if (cfType2 && "cf2".equals(asset.getType())) {
-                                fileName = asset.getName();
+                                selectedAsset = asset;;
                                 break;
                             } else if (!cfType2 && "cf1".equals(asset.getType())) {
-                                fileName = asset.getName();
+                                selectedAsset = asset;;
                             }
                         }
                     }
 
-                    File sdcard = Environment.getExternalStorageDirectory();
-                    File firmwareFile = new File(sdcard, mDownloadDirectory + "/" + fileName);
-                    if (firmwareFile == null || !firmwareFile.exists()) {
-                        return "Problems with downloaded firmware files.";
+                    if (!mFirmwareDownloader.isFileAlreadyDownloaded(selectedAsset)) {
+                        return "Problem with downloaded firmware files.";
                     }
                     long startTime = System.currentTimeMillis();
 //                    bootloader.flash(firmwareFile, TargetTypes.STM32);
---
+++ src/se/bitcraze/crazyfliecontrol/bootloader/FirmwareDownloader.java
@@ -0,0 +1,171 @@
+package se.bitcraze.crazyfliecontrol.bootloader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import se.bitcraze.crazyfliecontrol.bootloader.Firmware.Asset;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Environment;
+import android.util.Log;
+
+public class FirmwareDownloader {
+
+    private static final String LOG_TAG = "FirmwareDownloader";
+    private Context mContext;
+    private String mDownloadDirectory = "CrazyflieControl";
+    private List<Firmware> mFirmwares = new ArrayList<Firmware>();
+
+    public FirmwareDownloader(Context context) {
+        this.mContext = context;
+    }
+
+    public void checkForFirmwareUpdate() {
+        String releaseURL = "https://api.github.com/repos/bitcraze/crazyflie-firmware/releases";
+        new DownloadWebpageTask().execute(releaseURL);
+    }
+
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                String input = downloadUrl(urls[0]);
+                mFirmwares = parseJson(input);
+
+                return "Status: Found " + mFirmwares.size() + " firmwares.";
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            } catch (JSONException e) {
+                return "Error during parsing JSON content.";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            ((BootloaderActivity) mContext).updateFirmwareSpinner(mFirmwares);
+            ((BootloaderActivity) mContext).setStatusLine(result);
+        }
+    }
+
+    public void downloadFirmware(Firmware selectedFirmware) {
+        if (selectedFirmware != null && selectedFirmware.getAssets().size() > 0) {
+            for (Asset asset : selectedFirmware.getAssets()) {
+                //check if file is already downloaded
+                if (isFileAlreadyDownloaded(asset)) {
+                    Log.d(LOG_TAG, "File " + asset.getName() + " already downloaded.");
+                } else {
+                    String browserDownloadUrl = asset.getBrowserDownloadUrl();
+                    downloadFile(browserDownloadUrl, asset.getName());
+                }
+            }
+        } else {
+            Log.d(LOG_TAG, "Selected firmware does not have assets.");
+            ((BootloaderActivity) mContext).setStatusLine("Selected firmware does not have assets.");
+            return;
+        }
+    }
+
+    public boolean isFileAlreadyDownloaded (Asset asset) {
+        int assetSize = asset.getSize();
+        File sdcard = Environment.getExternalStorageDirectory();
+        File firmwareFile = new File(sdcard, mDownloadDirectory + "/" + asset.getName());
+        return firmwareFile.exists() && firmwareFile.length() == assetSize;
+    }
+
+    public void downloadFile (String url, String fileName) {
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+        request.setDescription("Some description");
+        request.setTitle(fileName);
+        // in order for this if to run, you must use the android 3.2 to compile your app
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            request.allowScanningByMediaScanner();
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        }
+        request.setDestinationInExternalPublicDir(mDownloadDirectory, fileName);
+
+        // get download service and enqueue file
+        DownloadManager manager = (DownloadManager) mContext.getSystemService(Context.DOWNLOAD_SERVICE);
+        manager.enqueue(request);
+    }
+
+    private String downloadUrl(String myUrl) throws IOException {
+        BufferedReader reader = null;
+        StringBuilder builder = new StringBuilder();
+        HttpClient client = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(myUrl);
+        try {
+            HttpResponse response = client.execute(httpGet);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();
+
+            if (statusCode == 200) {
+                HttpEntity entity = response.getEntity();
+                InputStream content = entity.getContent();
+                reader = new BufferedReader(new InputStreamReader(content, "UTF-8"));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
+                }
+            } else {
+                Log.d(LOG_TAG, "The response is: " + response);
+                return "The response is: " + response;
+            }
+        } finally {
+            // Makes sure that the InputStream is closed after the app is finished using it.
+            if (reader != null) {
+                reader.close();
+            }
+        }
+        return builder.toString();
+    }
+
+    public List<Firmware> parseJson(String input) throws JSONException {
+
+        List<Firmware> firmwares = new ArrayList<Firmware>();
+
+        JSONArray releasesArray = new JSONArray(input);
+        for (int i = 0; i < releasesArray.length(); i++) {
+            JSONObject releaseObject = releasesArray.getJSONObject(i);
+            String tagName = releaseObject.getString("tag_name");
+            String name = releaseObject.getString("name");
+            String createdAt = releaseObject.getString("created_at");
+            Firmware firmware = new Firmware(tagName, name, createdAt);
+            JSONArray assetsArray = releaseObject.getJSONArray("assets");
+            if (assetsArray != null && assetsArray.length() > 0) {
+                for (int n = 0; n < assetsArray.length(); n++) {
+                    JSONObject assetsObject = assetsArray.getJSONObject(n);
+                    String assetName = assetsObject.getString("name");
+                    int size = assetsObject.getInt("size");
+                    String downloadURL = assetsObject.getString("browser_download_url");
+                    Asset asset = new Firmware().new Asset(assetName, size, downloadURL);
+                    firmware.addAsset(asset);
+                }
+            }
+            firmwares.add(firmware);
+        }
+        return firmwares;
+    }
+
+}
